0760f0882ea5570fcafe77e93d5b96b6b3695019
Mohitpg <mohitp.gupta1102@gmail.com>
2023-12-28 10:41:38 +0530

    Added packages and client side routing components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Home from './Home';
+import Gallery from './Gallery';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Navbar variant='light'>
+        <Container >
+          <Nav className="mx-auto">
+            <Navbar.Brand><Link to="/">Home</Link></Navbar.Brand>
+            <Navbar.Brand><Link to="/gallery">Your Collections</Link></Navbar.Brand>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/gallery" element={<Gallery />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/Home.js
@@ -0,0 +1,4 @@
+function Home(){
+    return <h1>Yolo</h1>
+}
+export default Home
