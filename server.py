0e0480b12833ea6ecbe00a626b16206d6610f169
Mohitpg <mohitp.gupta1102@gmail.com>
2024-01-16 19:52:07 +0530

    Updated for docker environment

--- server.py
+++ server.py
@@ -13,9 +13,9 @@ import io
 from base64 import encodebytes
 
 from pymongo import MongoClient
-# client= MongoClient("localhost",27017)
-# db = client["neuralvids"]
-# col=db["vids"]
+client= MongoClient("mongo",27017)
+db = client["neuralvids"]
+col=db["vids"]
 
 app=Flask(__name__,static_folder="frontend/build/static",template_folder="frontend/build")
 CORS(app)
